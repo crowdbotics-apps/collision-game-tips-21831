ebea353e017c0923b7bb3ffa88c40456f0392184
Crowdbotics <team@crowdbotics.com>
2020-10-22 16:11:35 +0000

    Update models

--- backend/collision_game_tips_21831/wsgi.py
+++ backend/collision_game_tips_21831/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'collision_game_tips_21831.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "collision_game_tips_21831.settings")
 
 application = get_wsgi_application()
