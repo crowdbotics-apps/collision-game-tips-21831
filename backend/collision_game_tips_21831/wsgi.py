f07cec5ff9a8200a00249126a0d827024ed9f4ba
Crowdbotics <team@crowdbotics.com>
2020-10-22 16:10:50 +0000

    Initial commit.

---
+++ backend/collision_game_tips_21831/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for collision_game_tips_21831 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'collision_game_tips_21831.settings')
+
+application = get_wsgi_application()
